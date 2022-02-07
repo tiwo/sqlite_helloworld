4f6ac68bc51da16054047c01e90b51c911f27ca9
Thomas Wissen <tiwocode@gmail.com>
2022-02-07 03:40:44 +0100

    extension function helloworld()

--- sqlite_helloworld.c
+++ sqlite_helloworld.c
@@ -1,31 +1,50 @@
-/* Add your header comment here */
-#include <sqlite3ext.h> /* Do not use <sqlite3.h>! */
+#include <stddef.h>
+#include <sqlite3ext.h>
 SQLITE_EXTENSION_INIT1
 
-/* Insert your extension code here */
+
+static void helloFunc(
+    sqlite3_context *ctx,
+    int argc,
+    sqlite3_value **argv
+) {
+    sqlite3_result_text(ctx, "\u2764 Hello, world! \u2764", -1, SQLITE_TRANSIENT);
+}
 
 #ifdef _WIN32
 __declspec(dllexport)
 #endif
-/* TODO: Change the entry point name so that "extension" is replaced by
-** text derived from the shared library filename as follows:  Copy every
-** ASCII alphabetic character from the filename after the last "/" through
-** the next following ".", converting each character to lowercase, and
-** discarding the first three characters if they are "lib".
-*/
-int sqlite3_extension_init(
+
+int sqlite3_helloworld_init(
   sqlite3 *db, 
   char **pzErrMsg, 
   const sqlite3_api_routines *pApi
 ){
-  int rc = SQLITE_OK;
+
+  int error = SQLITE_OK;
   SQLITE_EXTENSION_INIT2(pApi);
-  /* Insert here calls to
-  **     sqlite3_create_function_v2(),
-  **     sqlite3_create_collation_v2(),
-  **     sqlite3_create_module_v2(), and/or
-  **     sqlite3_vfs_register()
-  ** to register the new features that your extension adds.
-  */
-  return rc;
+
+  error = sqlite3_create_function(
+    db,
+        /* sqlite *db */
+    "helloworld",
+        /* const char *zFunctionName */
+    0,
+        /* int nArg */
+    SQLITE_UTF8 | SQLITE_DETERMINISTIC | SQLITE_INNOCUOUS,
+        /* int eTextRep; flags: arguments' encoding, innocuos even if abused, deterministic */
+    NULL,
+        /* void *pApp; an arbitrary pointer */
+    helloFunc,
+        /* void (*xFunc)(sqlite3_context*,int,sqlite3_value**) */
+    NULL,
+        /* void (*xStep)(sqlite3_context*,int,sqlite3_value**) */
+    NULL
+        /* void (*xFinal)(sqlite3_context*) */
+  );
+  if (error != SQLITE_OK) {
+      return error;
+  }
+
+  return SQLITE_OK;
 }
